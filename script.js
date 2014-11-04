e1667833af023c5da7a9b6f9ed7f0a0742396964
mattbspector <mattbspector@yahoo.com>
2014-11-04 17:49:59 -0500

    class

--- script.js
+++ script.js
@@ -11,6 +11,30 @@ $(document).ready(function(){
 	})
 
 
+	$(".nextbutton2").click(function(){
+		$("#page2").animate({width:'toggle'},1000);
+		
+
+
+	})
+
+
+	$(".nextbutton3").click(function(){
+		$("#page3").animate({width:'toggle'},1000);
+		
+
+
+	})
+
+
+	$(".nextbutton4").click(function(){
+		$("#page4").animate({width:'toggle'},1000);
+		
+
+
+	})
+
+
 		
 
 
