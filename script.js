952dc1353cfbbdc4bd79f3f33f9ff6e66fadabd8
mattbspector <mattbspector@yahoo.com>
2014-11-13 12:59:10 -0500

    puuush

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
-
+	var totalscore = 0;
+	var finalcolor = "";
 
 
 
@@ -9,32 +10,33 @@ $(document).ready(function(){
 
 	$(".nextbutton1").click(function(){
 
-	var firstboxval = $("#firstbox").val();
-	var secondboxval = $("#secondbox").val();
-	if(firstboxval != secondboxval)
-	{
-		alert("email's do not match!");
-	}
+			var firstboxval = $("#firstbox").val();
+			var secondboxval = $("#secondbox").val();
+			if(firstboxval != secondboxval)
+			{
+				alert("email's do not match!");
+			}
 
-	else
-	{
+			else
+			{
 
-		$("#page1").animate({width:'toggle'},1000);
-	}
+				$("#page1").animate({width:'toggle'},1000);
+			}
 
 
 	})
 
 
 	$(".nextbutton2").click(function(){
+
+	   finalcolor = $("#color input[type = 'color']").val();
 		$("#page2").animate({width:'toggle'},1000);
-		
-
-
 	})
 
 
 	$(".nextbutton3").click(function(){
+
+		totalscore += parseInt($("#food input[type = 'radio']:checked").val());
 		$("#page3").animate({width:'toggle'},1000);
 		
 
@@ -43,11 +45,31 @@ $(document).ready(function(){
 
 
 	$(".nextbutton4").click(function(){
+		totalscore += parseInt($("#meat input[type = 'range']").val());
+		if(totalscore < 5)
+		{	
+			$("#page5 .animal").text("YOU ARE A COW");
+			  $("#page5 img").attr("src", "all-about-cows-1.jpg");
+
+			
+		}
+		else if(totalscore < 10)
+		{
+			$("#page5 .animal").text("YOU ARE A PUPPY");
+			$("#page5 img").attr("src", "cute_puppy.jpg");
+			
+		}
+		else
+		{
+			$("#page5 .animal").text("YOU ARE A LION");
+			$("#page5 img").attr("src", "lion.jpg");
+		}	
+		$("#page5").css("background-color", finalcolor);
 		$("#page4").animate({width:'toggle'},1000);
-		
 
-
+		console.log(totalscore);
 	})
+
 
 });
 		
